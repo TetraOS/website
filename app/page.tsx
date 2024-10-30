f9b985977c791019754c82b6a7589c86feb81ec6
Carson Schildt <128015493+carsonschildt@users.noreply.github.com>
2024-10-30 10:57:28 -0600

    Minor div centering error.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return ( 
-    <div align="center">
+    <div>
       <h1>Whoops! ⚠️</h1>
       <p>The TetraOS website is going under major development right now! Sorry for the inconvenience. 😔</p>
     </div>
